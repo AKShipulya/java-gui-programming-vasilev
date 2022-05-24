04fda98d6f692c94c6cdc7b7149ef2c607e040ba
Andrey <akshipulya@gmail.com>
2022-05-25 00:12:58 +0300

    24/05/2022-1

--- src/chapter1/InputWindowDemo.java
+++ src/chapter1/InputWindowDemo.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 public class InputWindowDemo {
 
     public static void main(String[] args) {
+
         String text;
         int mType = JOptionPane.INFORMATION_MESSAGE;
         String mCaption = "User choice";
@@ -17,12 +18,10 @@ public class InputWindowDemo {
             input = JOptionPane.showInputDialog(null, "Put a number", "Number", JOptionPane.WARNING_MESSAGE);
             int last = Integer.parseInt(input) % 10;
             String[] color = {"Red", "Yellow", "Green"};
+
             // TODO: 5/24/22 страница 27, еще раз все перепроверь!!!
-            input = (String) JOptionPane.showMessageDialog(null,
-                    "Let show color: ",
-                    image,
-                    color[color.length - 1]
-            );
+            input = (String) JOptionPane.showMessageDialog(null,"Let show color: ", color[color.length - 1], mType, image);
+
             if (input == null) {
                 throw new Exception();
             }
